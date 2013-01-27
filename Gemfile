f286f5d84b2471dd22f62fd469ce6dc6516b04db
Yeshaohong <ysllyfe@163.com>
2013-01-27 11:59:58 +0800

    初始化

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://ruby.taobao.org'
+gem 'rails', '3.0.19'
+gem 'paperclip'
+gem 'mysql2', '~> 0.2.17'
+gem 'jquery-rails'
+gem 'will_paginate','3.0.pre2'
+gem 'rmagick'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UeditorForRails::Application.config.secret_token = '4510e874877c355cab43bee3f94be1d32a5fd54324808e0c32067282cf780f1d862f0b369f5edb7d010f1def463319060ba587987414c7e5382ece0a9bc1f221'
